27081618ad6eec5de6e462e0d1097053636561fb
Tahrim19 <tarimbilal4@gmail.com>
2024-06-24 08:57:24 -0700

    hi

--- backend/server.js
+++ backend/server.js
@@ -31,58 +31,24 @@
 
 
 
-// require('dotenv').config();
-// const express = require('express');
-// const cors = require('cors');
-// const { getAllEvents, createEvent, updateEvent, deleteEvent, UserData, getUsers } = require('./eventController');
-
-// const app = express();
-// const port = 5000;
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // Health check route
-// app.get('/health', (req, res) => {
-//   res.send('Server is up and running!');
-// });
-
-// // Routes
-// app.get('/api/events', getAllEvents);
-// app.post('/api/events', createEvent);
-// app.put('/api/events/:id', updateEvent);
-// app.delete('/api/events/:id', deleteEvent);
-// app.post('/api/users', UserData);
-// app.get('/api/users', getUsers);
-
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
-
-
-
-
-const express = require("express");
-const cors = require("cors");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 const { getAllEvents, createEvent, updateEvent, deleteEvent, UserData, getUsers } = require('./eventController');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('Express on Vercel');
+// Health check route
+app.get('/health', (req, res) => {
+  res.send('Server is up and running!');
 });
 
+// Routes
 app.get('/api/events', getAllEvents);
 app.post('/api/events', createEvent);
 app.put('/api/events/:id', updateEvent);
@@ -90,6 +56,8 @@ app.delete('/api/events/:id', deleteEvent);
 app.post('/api/users', UserData);
 app.get('/api/users', getUsers);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
+
